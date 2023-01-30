8a4d774296b0012fa9e926680250dc7199c6da9a
CristianX <thecristianx@hotmail.com>
2023-01-29 19:42:30 -0500

    Commit inicial de ApiPedidos

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+var cors = require("cors");
+const database = require("./modulos/dbconect");
+
+// settings
+app.set('port', process.env.PORT || 3003);
+
+
+// PRUEBAS
+app.use('/apifactura', require('./src/ApiPedidos/Factura'));
+
+
+// USUARIOS
+app.use('/apinuevopedido', require('./src/ApiPedidos/Nuevopedido'));
+app.use('/apiclicarrito', require('./src/ApiPedidos/Clicarrito'));
+app.use('/apiverpedidos', require('./src/ApiPedidos/Clipedido'));
+app.use('/apiinfopedido', require('./src/ApiPedidos/Cliinfopedido'));
+
+// ADMINISTRADOR
+app.use('/apipedidos', require('./src/ApiPedidos/Crudpedidos'));
+app.use('/apiadminproducto', require('./src/ApiPedidos/Adminpedido')); //ApiAdminProducto hace referencia a pedidos
+
+
+// starting the serve
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
